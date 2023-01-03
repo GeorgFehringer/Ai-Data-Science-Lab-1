201cc73decb36934923d1e07765b5530b06e25fa
JuliusBreuss <92053461+JuliusBreuss@users.noreply.github.com>
2023-01-03 10:31:01 +0100

    fixed errors

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ def find_pos(number, array):
     for i in range(3):
         for j in range(3):
             if array[i][j] == number:
-                x = j
-                y = i
-                return x, y
+                xtest = j
+                ytest = i
+                return xtest, ytest
 
 
 def manhattan_distance():
@@ -19,7 +19,27 @@ def manhattan_distance():
     return sum
 
 
-def Inversions(arr):                                #8-Puzzle are only then solveable, if there is an even number of inversions. Therefore we count them before we try to solve the puzzle
+def get_hamming_distance(puzzle, goal):
+    sum = 0
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if puzzle[i, j] != goal[i, j]:
+                sum += 1
+    return sum
+
+
+def get_manhattan_distance(puzzle_array, goal_array):
+    man_distance = 0
+    for i in range(1, 9):
+        for i in range(1, 9):
+            x1, y1 = find_pos(i, puzzle_array)
+            x2, y2 = find_pos(i, goal_array)
+            man_distance = man_distance + (abs(x1 - x2) + abs(y1 - y2))
+    return man_distance
+
+
+def inversions(
+        arr):  # 8-Puzzle are only then solveable, if there is an even number of inversions. Therefore we count them before we try to solve the puzzle
     count = 0
 
     for i in range(0, 9):
@@ -30,7 +50,7 @@ def Inversions(arr):                                #8-Puzzle are only then solv
     return count
 
 
-def Solvability(puzzle):                            #For the Inversion Function to work, we need a 1D Array. Because the puzzle is an 2D Array we need to convert the data.
+def solvability(puzzle):  # For the Inversion Function to work, we need a 1D Array. Because the puzzle is an 2D Array we need to convert the data.
     count = 0
     arr = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
@@ -39,9 +59,9 @@ def Solvability(puzzle):                            #For the Inversion Function 
             arr[count] = [puzzle[i][j]]
             count += 1
 
-    inversions = Inversions(arr)                   #After converting the data to a 1D array we can count the number of Inversions
+    inversions_count = inversions(arr)  # After converting the data to a 1D array we can count the number of Inversions
 
-    if inversions % 2 == 0:                        #If there are an even amount of inversions, the puzzle is solveable
+    if inversions_count % 2 == 0:  # If there are an even amount of inversions, the puzzle is solvable
         return True
     else:
         return False
@@ -61,12 +81,13 @@ if __name__ == '__main__':
     for x in goal:
         print(x)
 
-    print(manhattan_distance())
+    print()
+    print(get_manhattan_distance(puzzle, goal))
 
     emptyX, emptyY = find_pos(0, puzzle)
     print(emptyX, emptyY)
 
-    if Solvability(puzzle):
-        print("Yes, solving continious")
+    if solvability(puzzle):
+        print("Yes, solving continues")
     else:
-        print("Puzzle is not solveable therefore programm is terminated")
+        print("Puzzle is not solvable, therefore program is terminated")
